0245bf6d4f55308774a7e4d3bc0bbc8679994e1b
Luis Filipe <luiiz_fillipe@hotmail.com>
2023-11-01 14:28:07 -0300

    fix: imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { CompressStringUseCase } from 'src/app/usecases/string-compress.usecase';
 import { DecompressStringUseCase } from 'src/app/usecases/string-decompress.usecase';
-import { AppController } from './controllers/app.controller';
+import { AppController } from './presentation/controllers/app.controller';
 
 @Module({
   imports: [],
